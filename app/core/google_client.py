91589488f50902e8a257c1e304335cffaf9f2215
Kuznetsov Sergey <monteg179@yandex.ru>
2023-12-27 04:35:22 +0300

    version 0.1

--- app/core/google_client.py
+++ app/core/google_client.py
@@ -1,5 +1,3 @@
-import json
-
 from aiogoogle import Aiogoogle
 from aiogoogle.auth.creds import ServiceAccountCreds
 
@@ -10,7 +8,6 @@ SCOPES = [
     'https://www.googleapis.com/auth/drive'
 ]
 
-# tests only
 INFO = {
     'type': settings.type,
     'project_id': settings.project_id,
@@ -24,22 +21,7 @@ INFO = {
     'client_x509_cert_url': settings.client_x509_cert_url
 }
 
-with open(settings.credentials, 'r') as file:
-    info = json.load(file)
-
-cred = ServiceAccountCreds(
-    scopes=SCOPES,
-    type=info['type'],
-    project_id=info['project_id'],
-    private_key_id=info['private_key_id'],
-    private_key=info['private_key'],
-    client_email=info['client_email'],
-    client_id=info['client_id'],
-    auth_uri=info['auth_uri'],
-    token_uri=info['token_uri'],
-    auth_provider_x509_cert_url=info['auth_provider_x509_cert_url'],
-    client_x509_cert_url=info['client_x509_cert_url'],
-)
+cred = ServiceAccountCreds(scopes=SCOPES, **INFO)
 
 
 async def get_service():
